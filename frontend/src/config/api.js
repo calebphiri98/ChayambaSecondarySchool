fe09e09ffc8c83f43a2866edf0641f28e39b29fa
calebphiri98 <calebphiri98@gmail.com>
2026-06-22 08:02:33 +0200

    added the config file

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
 
-const API_URL = "http://localhost:5000";
-// const API_URL = "https://chayamba-backend.onrender.com";
+// const API_URL = "http://localhost:5000";
+ const API_URL = "https://chayambasecondaryschool.onrender.com";
 
 export default API_URL;
